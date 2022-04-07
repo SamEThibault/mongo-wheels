42d18e433d5cb59218f20cda9ec040a4ed7e4a35
Samuel Emard-Thibault <sam5thibault@gmail.com>
2022-04-07 13:36:07 -0400

    initial commit: spring project

---
+++ mongoWheels/src/main/java/com/ms/mongowheels/MongoWheelsApplication.java
@@ -0,0 +1,15 @@
+package com.ms.mongowheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoWheelsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MongoWheelsApplication.class, args);
+    }
+
+}
---
+++ mongoWheels/src/test/java/com/ms/mongowheels/MongoWheelsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ms.mongowheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoWheelsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
