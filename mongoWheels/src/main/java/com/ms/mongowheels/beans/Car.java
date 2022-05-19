ce4b766e97de1cbe560841b72bbf68006339450e
Samuel Emard-Thibault <92962712+SamEThibault@users.noreply.github.com>
2022-05-19 00:58:21 -0400

    modified car bean structure

--- mongoWheels/src/main/java/com/ms/mongowheels/beans/Car.java
+++ mongoWheels/src/main/java/com/ms/mongowheels/beans/Car.java
@@ -19,59 +19,37 @@ public class Car {
 	@Id
 	private String id;
 
+	private int height;
+	private int length;
+	private int width;
+	private int cityMpg;
+	private int highwayMpg;
+	private int year;
+	private int hp;
+	private int torque;
+	private int numGears;
+	private boolean hybrid;
 
 	@NonNull
-	private Dimensions dimensions;
-
-	// this instance includes engine stats object as a field
-	@NonNull
-	private Engine engine;
-
+	private String driveLine;
 
 	@NonNull
-	private FuelInfo fuelInfo;
+	private String engineType;
 
 	@NonNull
-	private Identification identification;
+	private String transmission;
 
-	// no argument constructor initializing fields to default values
-//	public Car()
-//	{
-//		id = "not found";
-//		dimensions = new Dimensions();
-//		engine = new Engine();
-//		fuelInfo = new FuelInfo();
-//		identification = new Identification();
-//	}
-//
-//	// constructor, using bean copy constructors for deep copies of mutable objects
-//	public Car(String idIn, Dimensions dimensionsIn, Engine engineIn,
-//			   FuelInfo fuelInfoIn, Identification identificationIn)
-//	{
-//		// in this case, all instance variables are immutable so no copy constructor calls needed yet
-//		id = idIn;
-//		dimensions = dimensionsIn;
-//		engine = engineIn;
-//		fuelInfo = fuelInfoIn;
-//		identification = identificationIn;
-//	}
-//
-//	// constructor with all instance variable values from all other objects
-//	public Car(int heightIn, int lengthIn, int widthIn, String driveLineIn, String engineTypeIn, boolean hybridIn, int numGearsIn,
-//			   String transmissionIn, int horsepowerIn, int torqueIn, int cityMpgIn, String fuelTypeIn, int highwayMpgIn,
-//			   String classificationIn, String IDIn, String makeIn, String modelYearIn, int yearIn)
-//	{
-//		dimensions = new Dimensions(heightIn, lengthIn, widthIn);
-//
-//		engine = new Engine(driveLineIn, engineTypeIn, hybridIn, numGearsIn, transmissionIn,
-//							new EngineStats(horsepowerIn, torqueIn));
-//
-//		fuelInfo = new FuelInfo(cityMpgIn, fuelTypeIn, highwayMpgIn);
-//
-//		identification = new Identification(classificationIn, IDIn, makeIn, modelYearIn, yearIn);
-//
-//	}
+	@NonNull
+	private String fuelType;
 
+	@NonNull
+	private String classification;
+
+	@NonNull
+	private String info;
+
+	@NonNull
+	private String make;
 }
 
 
--- mongoWheels/src/main/java/com/ms/mongowheels/repositories/CarRepository.java
+++ mongoWheels/src/main/java/com/ms/mongowheels/repositories/CarRepository.java
@@ -1,21 +1,8 @@
 package com.ms.mongowheels.repositories;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import com.ms.mongowheels.beans.Car;
-import com.ms.mongowheels.beans.Dimensions;
 
 
-public interface CarRepository extends MongoRepository<Car, String> {
-
-	
-	@Query(value="{}", fields = "{'info':1}")
-	public List<Dimensions> getDimensions();
-
-
-	
-	
-}
+public interface CarRepository extends MongoRepository<Car, String> { }
