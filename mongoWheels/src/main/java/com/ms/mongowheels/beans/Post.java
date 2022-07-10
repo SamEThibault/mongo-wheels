45c3f6e05b9ccdb7db3a89f46141fbebd29ee4cc
Matthieu Constant <91146150+MattConstant@users.noreply.github.com>
2022-07-10 00:15:50 -0400

    Backend for Community Section
    Created bean and repo for this bean

---
+++ mongoWheels/src/main/java/com/ms/mongowheels/beans/Post.java
@@ -0,0 +1,29 @@
+package com.ms.mongowheels.beans;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Document(collection="discussion")
+public class Post {
+	
+	@Id
+	private String id;
+	@NonNull
+	private String username;
+	@NonNull
+	private String message;
+	
+	private String date;
+	
+}
