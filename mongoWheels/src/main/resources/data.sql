f7b8d1a89e85f9712e84b4426e12629a1e06d9d1
Matthieu Constant <91146150+MattConstant@users.noreply.github.com>
2022-07-13 00:16:05 -0400

    Update Data.sql
    Changed the order for the Roles

--- mongoWheels/src/main/resources/data.sql
+++ mongoWheels/src/main/resources/data.sql
@@ -3,12 +3,13 @@ values ('admin', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu',
  
 insert into SEC_User (userName, encryptedPassword, ENABLED)
 values ('user', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', 1);
+
+insert into sec_role (roleName)
+values ('ROLE_USER');
  
 insert into sec_role (roleName)
 values ('ROLE_ADMIN');
  
-insert into sec_role (roleName)
-values ('ROLE_USER');
 
 insert into user_role (userId, roleId)
 values (1, 1);
